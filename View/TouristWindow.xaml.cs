d915bb261bcba7774dc3c334ce8ddaaec384d11e
MilanUD <149141569+MilanUD@users.noreply.github.com>
2024-03-14 01:12:28 +0100

    Started implementation of displaying tours

--- View/TouristWindow.xaml.cs
+++ View/TouristWindow.xaml.cs
@@ -22,59 +22,22 @@ namespace BookingApp.View
     /// </summary>
     public partial class TouristWindow : Window
     {
-        public static ObservableCollection<Comment> Comments { get; set; }
+        public static ObservableCollection<Tour> Tours { get; set; }
 
-        public Comment SelectedComment { get; set; }
+        public Tour SelectedTour { get; set; }
 
         public User LoggedInUser { get; set; }
 
-        private readonly CommentRepository _repository;
+        private readonly TourRepository _repository;
 
         public TouristWindow(User user)
         {
             InitializeComponent();
             DataContext = this;
             LoggedInUser = user;
-            _repository = new CommentRepository();
-            Comments = new ObservableCollection<Comment>(_repository.GetByUser(user));
+            _repository = new TourRepository();
+            Tours = new ObservableCollection<Tour>(_repository.GetAll());
         }
 
-        private void ShowCreateCommentForm(object sender, RoutedEventArgs e)
-        {
-            CommentForm createCommentForm = new CommentForm(LoggedInUser);
-            createCommentForm.Show();
-        }
-
-        private void ShowViewCommentForm(object sender, RoutedEventArgs e)
-        {
-            if (SelectedComment != null)
-            {
-                CommentForm viewCommentForm = new CommentForm(SelectedComment);
-                viewCommentForm.Show();
-            }
-        }
-
-        private void ShowUpdateCommentForm(object sender, RoutedEventArgs e)
-        {
-            if (SelectedComment != null)
-            {
-                CommentForm updateCommentForm = new CommentForm(SelectedComment, LoggedInUser);
-                updateCommentForm.Show();
-            }
-        }
-
-        private void Delete(object sender, RoutedEventArgs e)
-        {
-            if (SelectedComment != null)
-            {
-                MessageBoxResult result = MessageBox.Show("Are you sure?", "Delete comment",
-                    MessageBoxButton.YesNo, MessageBoxImage.Question);
-                if (result == MessageBoxResult.Yes)
-                {
-                    _repository.Delete(SelectedComment);
-                    Comments.Remove(SelectedComment);
-                }
-            }
-        }
     }
 }

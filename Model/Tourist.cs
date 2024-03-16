264febb131034c6db8be12a71c1b72b9a2e1a721
MilanUD <149141569+MilanUD@users.noreply.github.com>
2024-03-16 19:03:22 +0100

    Implemented tour reservation

--- Model/Tourist.cs
+++ Model/Tourist.cs
@@ -1,17 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Cache;
 using System.Text;
 using System.Threading.Tasks;
+using BookingApp.Serializer;
 
 namespace BookingApp.Model
 {
-    public class Tourist
+    public class Tourist : ISerializable
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string LastName { get; set; }
 
         public int Age { get; set; }
+
+        public Tourist()
+        {
+
+        }
+
+        public Tourist(string name, string lastName, int age)
+        {
+            Name = name;
+            LastName = lastName;
+            Age = age;
+        }
+
+        public string[] ToCSV()
+        {
+            string[] csvValues =
+            {
+                Id.ToString(),
+                Name,
+                LastName,
+                Age.ToString()
+
+            };
+            return csvValues;
+        }
+
+        public void FromCSV(string[] values)
+        {
+            Id = int.Parse(values[0]);
+            Name = values[1];
+            LastName = values[2];
+            Age = int.Parse(values[3]);
+
+        }
     }
+
+    
 }
---
+++ Repository/TourReservationRepository.cs
@@ -0,0 +1,44 @@
+﻿using BookingApp.Model;
+using BookingApp.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Repository
+{
+    public class TourReservationRepository
+    {
+        private const string FilePath = "../../../Resources/Data/tourReservation.csv";
+
+        private readonly Serializer<TourReservation> _serializer;
+
+        private List<TourReservation> _tourReservation;
+
+        public TourReservationRepository()
+        {
+            _serializer = new Serializer<TourReservation>();
+            _tourReservation = _serializer.FromCSV(FilePath);
+        }
+
+        public int NextId()
+        {
+            _tourReservation = _serializer.FromCSV(FilePath);
+            if (_tourReservation.Count < 1)
+            {
+                return 1;
+            }
+            return _tourReservation.Max(c => c.Id) + 1;
+        }
+
+        public TourReservation Save(TourReservation tourReservation)
+        {
+            tourReservation.Id = NextId();
+            _tourReservation = _serializer.FromCSV(FilePath);
+            _tourReservation.Add(tourReservation);
+            _serializer.ToCSV(FilePath, _tourReservation);
+            return tourReservation;
+        }
+    }
+}

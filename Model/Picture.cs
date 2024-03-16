be426a47de84800f0b5238d400d36c0cc702673f
MilanUD <149141569+MilanUD@users.noreply.github.com>
2024-03-16 22:00:17 +0100

    Picture and PictureRepository creation

---
+++ Model/Picture.cs
@@ -0,0 +1,38 @@
+﻿using BookingApp.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Model
+{
+    public class Picture : ISerializable
+    {
+        public int Id { get; set; }
+        public string Path { get; set; }
+
+        public int TourId {  get; set; }
+
+        public Picture() { }
+
+        public void FromCSV(string[] values)
+        {
+            Id = int.Parse(values[0]);
+            Path = values[1];
+            TourId = int.Parse(values[2]);
+           
+        }
+
+        public string[] ToCSV()
+        {
+            string[] csvValues = {
+                Id.ToString(),
+                Path,
+                TourId.ToString()
+            };
+            return csvValues;
+
+        }
+    }
+}

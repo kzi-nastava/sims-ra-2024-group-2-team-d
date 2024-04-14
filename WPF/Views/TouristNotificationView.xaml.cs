5b900b920add8a904a49ccd5055c682f8d6e3d37
MilanUD <149141569+MilanUD@users.noreply.github.com>
2024-04-14 15:20:03 +0200

    Implemented notifications when added to tour

---
+++ WPF/Views/TouristNotificationView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using BookingApp.Model;
+using BookingApp.WPF.ViewModels;
+
+namespace BookingApp.WPF.Views
+{
+    /// <summary>
+    /// Interaction logic for TouristNotificationView.xaml
+    /// </summary>
+    public partial class TouristNotificationView : Window
+    {
+        public TouristNotificationView(List<TourInstance> activeTours, User loggedInUser)
+        {
+            InitializeComponent();
+            this.DataContext = new TouristNotificationViewModel(activeTours, loggedInUser);
+        }
+    }
+}

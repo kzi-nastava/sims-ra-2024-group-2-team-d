d5f1462c0e1d4e0eba6176282a4b8aee16350093
MilanUD <149141569+MilanUD@users.noreply.github.com>
2024-04-25 00:19:33 +0200

    Added class for tourRequest

---
+++ Model/TourRequest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookingApp.Serializer;
+
+namespace BookingApp.Model
+{
+    public enum Status { Accepted,OnHold, Invalid}
+    public class TourRequest : ISerializable
+    {
+        public int Id { get; set; }
+        public Status CurrentStatus { get; set; }
+        public string Location {  get; set; }
+        public string Description {  get; set; }
+        public string Language {  get; set; }
+        public int NumberOfTourists {  get; set; }
+        public DateOnly Start {  get; set; }
+        public DateOnly End { get; set; }
+        public DateOnly CreatedOn {  get; set; }
+
+        public TourRequest()
+        {
+
+        }
+
+        public string[] ToCSV()
+        {
+            string[] csvValues =
+            {
+                Id.ToString(),
+                CurrentStatus.ToString(),
+                Location,
+                Language,
+                Description,
+                NumberOfTourists.ToString(),
+                Start.ToString("yyyy-MM-dd"),
+                End.ToString("yyyy-MM-dd"),
+                CreatedOn.ToString("yyyy-MM-dd")
+
+            };
+            return csvValues;
+        }
+
+        public void FromCSV(string[] values)
+        {
+            Id = int.Parse(values[0]);
+            CurrentStatus = (Status)Enum.Parse(typeof(Status), values[1]);
+            Location = values[2];
+            Language = values[3];
+            Description = values[4];
+            NumberOfTourists = int.Parse(values[5]);
+            Start = DateOnly.Parse(values[6]);
+            End = DateOnly.Parse(values[7]);
+            CreatedOn = DateOnly.Parse(values[8]);
+
+        }
+
+
+
+    }
+}
---
+++ Repository/TourRequestRepository.cs
@@ -0,0 +1,44 @@
+﻿using BookingApp.Model;
+using BookingApp.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Repository
+{
+    public class TourRequestRepository
+    {
+        private const string FilePath = "../../../Resources/Data/tourRequest.csv";
+
+        private readonly Serializer<TourRequest> _serializer;
+
+        private List<TourRequest> _tourRequest;
+
+        public TourRequestRepository()
+        {
+            _serializer = new Serializer<TourRequest>();
+            _tourRequest = _serializer.FromCSV(FilePath);
+        }
+
+        public int NextId()
+        {
+            _tourRequest = _serializer.FromCSV(FilePath);
+            if (_tourRequest.Count < 1)
+            {
+                return 1;
+            }
+            return _tourRequest.Max(c => c.Id) + 1;
+        }
+
+        public TourRequest Save(TourRequest tourRequest)
+        {
+            tourRequest.Id = NextId();
+            _tourRequest = _serializer.FromCSV(FilePath);
+            _tourRequest.Add(tourRequest);
+            _serializer.ToCSV(FilePath, _tourRequest);
+            return tourRequest;
+        }
+    }
+}
